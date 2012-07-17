c35ceddb1bebc0b058a043fd8836174ceb67c044
nivco <nivco@localhost>
2012-07-17 03:05:35 +0200

    Updating the Java sample to the Sample using API v2. Also adding /#/create/<folderID> URL handler and support for setting the folder when creating a new file.

--- python/static/js/app.js
+++ python/static/js/app.js
@@ -13,7 +13,7 @@ google.load('picker', '1');
 
 angular.module('app', ['app.filters', 'app.services', 'app.directives'])
     .constant('saveInterval', 15000)
-    .constant('appId', [[YOUR_APP_ID]])
+    .constant('appId', [[YOUR_APP_ID]]) // Please replace this with your Application ID.
     .config(['$routeProvider', function ($routeProvider) {
     $routeProvider
         .when('/edit/:id', {templateUrl:'partials/editor.html', controller:EditorCtrl})
